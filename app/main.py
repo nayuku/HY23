1067dc478f0644f2bfef9e1f0c39bf43c6b81cf5
nayuku <rhag.nay@protonmail.com>
2023-09-28 22:56:01 +0200

    first commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_home():
+    return templates.TemplateResponse("home.html", {"request": {}})
+
+
+@app.get("/browser", response_class=HTMLResponse)
+async def read_browser():
+    return templates.TemplateResponse("browser.html", {"request": {}})
+
+
+@app.get("/about", response_class=HTMLResponse)
+async def read_about():
+    return templates.TemplateResponse("about.html", {"request": {}})
