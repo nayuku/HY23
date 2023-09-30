245eb482badcaf1b75f3f4ace739c4299913db0c
Wojciech Grzyb <wgrzyb12@gmail.com>
2023-10-01 00:58:18 +0200

    Commit

--- flask_app/main.py
+++ flask_app/main.py
@@ -33,7 +33,6 @@ def get_markets(market_dict='./data/markets.json'):
 @app.route('/calculator1', methods=['GET', 'POST'])
 def calculator1():
     if request.method == 'POST':
-        print("---------------------------------")
         form_data = request.form.to_dict()
         keys_to_check = ['Case_number', 'Owner']
         if any(len(form_data[key]) != 100 for key in keys_to_check):
@@ -61,69 +60,32 @@ def calculator1():
                 prefix = key.split('-')[0]
                 grouped_dict[prefix] += Decimal(value)
         result_dict.update(grouped_dict)
-        session['form_data'] = result_dict
+        session['form_data1'] = result_dict
         return redirect(url_for('calculator2'))
     return render_template('calculator1.html', title='Kalkulator kryptowalut', crypto_fields=get_crypto_fields())
 
 
 @app.route('/calculator2', methods=['GET', 'POST'])
 def calculator2():
-    form_data = session.get('form_data', {})
-    print(form_data)
+    form_data = session.get('form_data1', {})
     if request.method == 'POST':
         if len(request.form) == 0:
             flash(f"Nie wybranego żadnego dostawcy danych", 'alert alert-danger')
             return redirect(request.url)
-        print(request.form.to_dict())
+        session['form_data2'] = request.form.to_dict()
         return redirect(url_for('show_result'))
-    assets = {key.replace(" ", "_"): value for key, value in form_data.items()
-              if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
+    assets = {key.replace(" ", "_"): value for key, value in form_data.items() if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
     return render_template('calculator2.html', title='Kalkulator kryptowalut', assets=assets, markets=get_markets(),
                            form_data=form_data)
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def show_result():
-    species = json.loads(request.args['species'])
-    return render_template('result.html', title='Wyniki', species=species)
-
-
-def get_form_fields():
-    fields = {
-        "TYPE STR": {
-            "id": "str_var",
-            "type": "str",
-            "required": True
-        },
-        "TYPE SELECT": {
-            "id": "select_var",
-            "type": "select",
-            "values": ["Najmniejszej troski", "Nierozpoznane", "Wymarle","Zagrozone"],
-            "required": True
-        },
-        "TYPE BOOL": {
-            "id": "bool_var",
-            "type": "bool",
-            "required": False
-        },
-        "TYPE INT": {
-            "id": "int_var",
-            "type": "int",
-            "required": False
-        }
-    }
-    return fields
-
-
-@app.route('/manual_data_provider', methods=['GET', 'POST'])
-def add_species():
-    if request.method == 'POST':
-        print(request.form)  # Wypisanie parametrow z formularza
-        gatunek = request.form['gatunek'].replace(' ', '_')
-        sposob_odz = request.form['sposob_odzywiania']
-        flash(f"Dane zostały dodane", 'alert alert-success')
-
-    return render_template('manual.html', title='Manualne wprowadzanie danych', form_fields=get_form_fields())
+    form_data1 = session.get('form_data1', {})
+    form_data2 = session.get('form_data2', {})
+    print(form_data1)
+    print(form_data2)
+    return render_template('about.html', title='Wyniki')
 
 
 @app.route('/update_directory', methods=['GET'])
