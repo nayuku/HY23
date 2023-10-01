278bb39bf288725d2748604952b0c2873c0b727e
Wojciech Grzyb <wgrzyb12@gmail.com>
2023-10-01 05:56:43 +0200

    Commit

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,5 +1,7 @@
 import json
 import re
+import uuid
+import datetime
 
 from flask import Flask, render_template, request, flash, url_for, session
 from werkzeug.utils import redirect
@@ -67,14 +69,95 @@ def calculator1():
     return render_template('calculator1.html', title='Kalkulator kryptowalut', crypto_fields=get_crypto_fields())
 
 
+def calculate_manual_market_values(assets, value):
+    market = json.loads(value)
+    # TODO: pobierać kurs USD z api NBP
+    usd_to_pln = Decimal(4.3)
+    manual_market_values = {"name": market["name"] + " (" + market["address_url"] + ")", "type": "manual"}
+    for asset in assets:
+        if asset not in list(market.keys()):
+            # TODO: zwrócić komunikat błędu, bo nie powinniśmy tu trafić, trzeba zawsze określić kurs dla każdego kryptoaktywa
+            break
+        else:
+            rate = market[asset].split(" ")[0]
+            currency = market[asset].split(" ")[1]
+            value = Decimal(assets[asset])*Decimal(rate)
+            if currency == "USD":
+                value = value * usd_to_pln
+            manual_market_values[asset] = {"amount": assets[asset], "rate": rate, "currency": currency, "usd_to_pln": usd_to_pln, "value": value}
+    return manual_market_values
+
+
+def calculate_auto_market_values(assets, value):
+    # TODO: pobierać kurs USD z api NBP
+    usd_to_pln = Decimal(4.3)
+    auto_market_values = {"name": value, "type": "auto"}
+    data_provider = None
+    if value.startswith("Binance"):
+        data_provider = BinanceSpotDataProvider()
+    elif value.startswith("Kucoin"):
+        data_provider = KucoinSpotDataProvider()
+    elif value.startswith("Zonda"):
+        # TODO: uzupełnić o data provider-a Zondy
+        pass
+    else:
+        # TODO: zwrócić komunikat błędu, bo nie wspieramy danego data provider-a
+        return None
+    for asset in assets:
+        currency = "PLN"
+        rate = data_provider.get_rate_value(asset, currency)
+        if rate is None:
+            currency = "USD"
+            rate = data_provider.get_rate_value(asset, currency)
+        if rate is None:
+            # Giełda nie obraca danym kryptoaktywem
+            break
+        else:
+            value = Decimal(assets[asset]) * Decimal(rate)
+            if currency == "USD":
+                value = value * usd_to_pln
+            auto_market_values[asset] = {"amount": assets[asset], "rate": rate, "currency": currency, "usd_to_pln": usd_to_pln, "value": value}
+    return auto_market_values
+
+
+def calculate_portfolio_values(form_data1, form_data2):
+    assets = {key: value for key, value in form_data1.items() if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
+    portfolio_values = []
+    for key, value in form_data2.items():
+        if key.startswith('manual_data_'):
+            portfolio_values.append(calculate_manual_market_values(assets, value))
+        else:
+            portfolio_values.append(calculate_auto_market_values(assets, value))
+    print(portfolio_values)
+    return portfolio_values
+
+
+def is_portfolio_values_correct(form_data1, portfolio_values):
+    assets = {key: value for key, value in form_data1.items() if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
+    for asset in assets:
+        valid_flag = False
+        for values in portfolio_values:
+            if asset in list(values.keys()):
+                valid_flag = True
+                break
+        if not valid_flag:
+            return False
+    return True
+
+
 @app.route('/calculator2', methods=['GET', 'POST'])
 def calculator2():
     form_data = session.get('form_data1', {})
     if request.method == 'POST':
-        if len(request.form) == 0:
-            flash(f"Nie wybranego żadnego dostawcy danych", 'alert alert-danger')
+        if len(request.form) < 3:
+            flash(f"Nie zdefiniowano minimum 3 dostawców danych", 'alert alert-danger')
+            return redirect(request.url)
+        portfolio_values = calculate_portfolio_values(form_data, request.form.to_dict())
+        if not is_portfolio_values_correct(form_data, portfolio_values):
+            flash(f"Wybrani dostawcy danych nie obracają przynajmniej jedną z badanych kryptowalut. Zdefiniuj manualnie kursy kryptowalut.", 'alert alert-danger')
             return redirect(request.url)
         session['form_data2'] = request.form.to_dict()
+        session['portfolio_values'] = portfolio_values
         return redirect(url_for('show_result'))
     assets = {key.replace(" ", "_"): value for key, value in form_data.items() if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
     return render_template('calculator2.html', title='Kalkulator kryptowalut', assets=assets, markets=get_markets(),
@@ -85,13 +168,11 @@ def calculator2():
 def show_result():
     form_data1 = session.get('form_data1', {})
     form_data2 = session.get('form_data2', {})
-    print(form_data1)
-    print(form_data2)
-    binance_data_provider = BinanceSpotDataProvider()
-    kucoin_data_provider = KucoinSpotDataProvider()
-    print(binance_data_provider.get_rate_value("Bitcoin", "USD"))
-    print(kucoin_data_provider.get_rate_value("Bitcoin", "USD"))
-    return render_template('about.html', title='Wyniki')
+    portfolio_values = session.get('portfolio_values', {})
+    assets = {key: value for key, value in form_data1.items() if key not in ('Case_number', 'Enforcement_authority', 'Owner')}
+    return render_template('result.html', title='Szacowanie wartości kryptoaktywów', raport_id=str(uuid.uuid4()),
+                           raport_date=datetime.datetime.now().strftime("%d.%m.%Y"), form_data1=form_data1,
+                           form_data2=form_data2, portfolio_value=0)
 
 
 @app.route('/update_directory', methods=['GET'])
